5ffcf9fb526953b80352824692bc962d73ce43c2
isabel <isabel.wingen@innoq.com>
2022-03-08 11:34:32 +0100

    Day 14

---
+++ src/main/kotlin/Day14.kt
@@ -0,0 +1,99 @@
+import kotlin.math.pow
+
+private fun <T> parseInput(path: String, transform: (Pair<String, String>) -> T): List<T> {
+    return getResourceAsList(path)
+        .filter { it.isNotBlank() }
+        .map { it.split(" = ") }
+        .map { it[0] to it[1] }
+        .map { transform(it) }
+}
+
+private fun transformInputPairPart1(pair: Pair<String, String>): Pair<Int, String> {
+    return if (pair.first == "mask") {
+        -1 to pair.second
+    } else {
+        pair.first.substring(4).dropLast(1).toInt() to Integer.toBinaryString(pair.second.toInt()).padStart(36, '0')
+    }
+}
+
+private fun transformInputPairPart2(pair: Pair<String, String>): Pair<String, Long> {
+    return if (pair.first == "mask") {
+        pair.second to -1L
+    } else {
+        pair.first.substring(4).dropLast(1).toInt().toString(2).padStart(36, '0') to pair.second.toLong()
+    }
+}
+
+fun applyMaskPart1(mask: String, value: String): Long {
+    val res = value.toMutableList()
+    for (i in mask.indices) {
+        when (mask[i]) {
+            '0' -> res[i] = '0'
+            '1' -> res[i] = '1'
+            'X' -> res[i] = value[i]
+        }
+    }
+    return res.joinToString("").toLong(2)
+}
+
+fun applyMaskPart2(mask: String, value: String): List<Long> {
+    val res = value.toMutableList()
+    for (i in mask.indices) {
+        when (mask[i]) {
+            '0' -> res[i] = value[i]
+            '1' -> res[i] = '1'
+            'X' -> res[i] = 'X'
+        }
+    }
+    return getAllAddressesFromFloatingAddress(res.joinToString(""))
+}
+
+fun getAllAddressesFromFloatingAddress(address: String): List<Long> {
+    val floatingPoints = address.count { it == 'X' }
+    val pos = address
+        .mapIndexed { index, c -> index to c }
+        .filter { it.second == 'X' }
+        .map { it.first }
+    val res = mutableListOf<String>()
+    for (i in 0 until 2.0.pow(floatingPoints.toDouble()).toInt()) {
+        val bits = i.toString(2).padStart(floatingPoints, '0')
+        val newAd = address.toMutableList()
+        for (j in pos.indices) {
+            newAd[pos[j]] = bits[j]
+        }
+        res.add(newAd.joinToString(""))
+    }
+    return res
+        .map { it.toLong(2) }
+}
+
+fun executeDay14Part1(): Long {
+    val commands = parseInput("day14.txt") { transformInputPairPart1(it) }
+    var mask = ""
+    val mem = mutableMapOf<Int, Long>()
+    for (command in commands) {
+        if (command.first == -1) {
+            mask = command.second
+        } else {
+            mem[command.first] = applyMaskPart1(mask, command.second)
+        }
+    }
+    return mem.values.sum()
+}
+
+fun executeDay14Part2(): Long {
+    val commands = parseInput("day14.txt") { transformInputPairPart2(it) }
+    var mask = ""
+    val mem = mutableMapOf<Long, Long>()
+    for (command in commands) {
+        if (command.second == -1L) {
+            mask = command.first
+        } else {
+            val adds = applyMaskPart2(mask, command.first)
+            for (add in adds) {
+                mem[add] = command.second
+            }
+        }
+    }
+    return mem.values.sum()
+}
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -5,8 +5,4 @@ fun main(args: Array<String>) {
     // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
     println("Program arguments: ${args.joinToString()}")
 
-    val a = 186430471117411L
-    val b = 670237748696851L
-    println(extented_euclid(a, b))
-    println(executeDay13Part2())
 }
