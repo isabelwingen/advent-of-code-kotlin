1a6525e88a723b01fd2403511cb052d35518abb0
isabel <isabel.wingen@innoq.com>
2022-06-22 12:28:10 +0200

    Day18 WIP

--- src/main/kotlin/aoc2019/Day18.kt
+++ src/main/kotlin/aoc2019/Day18.kt
@@ -28,86 +28,135 @@ class Day18: Day("18") {
         return res.toList()
     }
 
-    data class Node(val value: Char, val coordinate: Pair<Int, Int>) {
+    data class Position(val row: Int, val column: Int) {
+
+        override fun toString(): String {
+            val x = row.toString().padStart(2, ' ')
+            val y = column.toString().padStart(2, ' ')
+            return "($x, $y)"
+        }
+    }
+
+    data class Node(val value: Char, val coordinate: Position) {
         override fun toString() = "$value/$coordinate"
     }
 
-    data class Edge(val start: Node, val goal: Node, val weight: Int = 1) {
-        override fun toString() = "$start ---$weight--> $goal"
+    data class Edge(val start: Position, val goal: Position, val weight: Int = 1) {
+        override fun toString(): String {
+            val n = weight.toString().length
+            val weightString = weight.toString().padStart(4, ' ').padEnd(5, ' ')
+            return "$start --$weightString--> $goal"
+        }
+    }
 
-        fun transformStartSymbol(): Edge {
-            val startSymbol = if (start.value == '@') '.' else start.value
-            val endSymbol = if (goal.value == '@') '.' else goal.value
-            return Edge(Node(startSymbol, start.coordinate), Node(endSymbol, goal.coordinate), weight)
+    data class Graph(val nodes: Set<Node>, val edges: Set<Edge>) {
+        override fun toString() = "  nodes:\n\t ${nodes.joinToString("\n\t ") { it.toString() }}" +
+                "\n  edges:\n\t ${edges.joinToString("\n\t ") { it.toString() }}"
+
+        fun openDoor(key: Char): Graph {
+            val n = nodes
+                .map { if (it.value == key) it.copy(value = '.') else it }
+                .map { if (it.value.lowercaseChar() == key) it.copy(value = '.') else it }
+                .toSet()
+            return this.copy(nodes = n)
         }
     }
 
 
-    private fun getRawGraph(name: String): MutableSet<Edge> {
+    private fun getRawGraph(name: String): Graph {
         val maze = readMaze(name)
         val rows = maze.size
         val cols = maze[0].size
-        val graph = mutableSetOf<Edge>()
+        val edges = mutableSetOf<Edge>()
+        val nodes = mutableSetOf<Node>()
         for (row in maze.indices) {
             for (col in maze[0].indices) {
-                val coord = row to col
+                val coord = Position(row, col)
                 val value = maze[row][col]
                 if (value == WALL) {
                     continue
                 } else {
+                    nodes.add(Node(value, coord))
                     for ((a,b) in getNeighbourIndices(row, col, rows, cols)) {
                         val neighbor = maze[a][b]
                         if (neighbor != WALL) {
-                            graph.add(Edge(Node(value, coord), Node(neighbor, a to b)))
+                            edges.add(Edge(coord,Position(a, b)))
+                            nodes.add(Node(neighbor, Position(a, b)))
                         }
                     }
                 }
             }
         }
-        return graph
+        return Graph(nodes.toSet(), edges.toSet())
     }
 
-    private fun neighbourEdges(graph: Set<Edge>, node: Node): List<Edge> {
-        return graph
-            .filter { it.start == node || it.goal == node}
-            .toList()
-    }
-
-    private fun simplifyGraph(graph: MutableSet<Edge>): MutableSet<Edge> {
-        val allNodes = graph.flatMap { setOf(it.start, it.goal) }.toSet()
-        val toBeRemoved = mutableSetOf<Node>()
-        for (node in allNodes) {
-            val n = neighbourEdges(graph, node)
-            if (node.value == EMPTY && n.size == 2) {
-                val (q, r) = n
-                val weight = q.weight + r.weight
-                val (c, d) = setOf(q.start, q.goal, r.start, r.goal).filter { it != node }.toList()
-                graph.add(Edge(c, d, weight))
-                toBeRemoved.add(node)
+    private fun simplifyGraph(graph: Graph): Graph {
+        val nodes = HashSet<Node>(graph.nodes)
+        val edges = HashSet<Edge>(graph.edges)
+        for (node in nodes.filter { it.value == '.' }) {
+            val neighbourEdges = edges.filter { it.goal == node.coordinate || it.start == node.coordinate }.toSet()
+            if (neighbourEdges.size == 2) {
+                val (a, b) = neighbourEdges.toList()
+                val (c, d) = setOf(a.goal, a.start, b.goal, b.start).filter { it != node.coordinate }.toList()
+                edges.add(Edge(c, d, a.weight + b.weight))
+                edges.removeIf { neighbourEdges.contains(it) }
+                nodes.remove(node)
             }
         }
-        graph.removeIf { toBeRemoved.contains(it.start) || toBeRemoved.contains(it.goal) }
-        return graph
+        edges.toList().forEach { edges.add(Edge(it.goal, it.start, it.weight)) }
+        return Graph(nodes.toSet(), edges.toSet())
     }
 
-    private fun getGraph(name: String): Pair<Node, Set<Edge>> {
-        var graph = getRawGraph(name)
-        var sizeBefore: Int
-        do {
-            sizeBefore = graph.size
-            graph = simplifyGraph(graph)
-        } while (graph.size != sizeBefore)
-        val start = graph.flatMap { setOf(it.goal, it.start) }.first { it.value == '@' }
-        val edgesWithStart = graph.filter { it.goal.value == '@' || it.start.value == '@' }
-        graph.removeIf { it.goal.value == '@' || it.start.value == '@' }
-        edgesWithStart.map { it.transformStartSymbol() }.forEach { graph.add(it) }
-        return start.copy(value = EMPTY) to graph.toSet()
+    private fun getGraph(name: String): Pair<Position, Graph> {
+        var g = getRawGraph(name)
+        var sizeBefore = 0
+        while (g.edges.size != sizeBefore) {
+            sizeBefore = g.edges.size
+            g = simplifyGraph(g)
+        }
+        val startNode = g.nodes.first { it.value == '@' }
+        val nodes = HashSet<Node>(g.nodes)
+        nodes.remove(startNode)
+        nodes.add(startNode.copy(value = '.'))
+        return startNode.coordinate to g.copy(nodes = nodes)
     }
+
+    data class State(val graph: Graph, val currentPosition: Position, val keys: Set<Char> = setOf(), val steps: Int = 0) {
+        override fun toString(): String {
+            return "\ngraph:\n$graph\ncurrent Position: $currentPosition\nkeys: $keys\nsteps: $steps\n"
+        }
+    }
+
+    fun <T> Set<T>.add(elem: T): Set<T> {
+        val s = this.toMutableSet()
+        s.add(elem)
+        return s.toSet()
+    }
+
+    private fun nextStates(state: State): Collection<State> {
+        val (graph, currentPosition, keys, steps) = state
+        val edges = graph.edges.filter { it.start ==  currentPosition }
+        return edges
+            .mapNotNull{ edge ->
+                val goalNode = graph.nodes.first { it.coordinate == edge.goal }
+                if (goalNode.value == '.') {
+                    State(graph, edge.goal, keys, steps)
+                } else if (goalNode.value.isLowerCase()) {
+                    val key = goalNode.value
+                    val r = State(graph.openDoor(key), edge.goal, keys.add(key), steps + edge.weight)
+                    r
+                } else {
+                    null
+                }
+            }
+     }
 
 
     override fun executePart1(name: String): Any {
-        val graph = getGraph(name)
-
+        val (s, g) = getGraph(name)
+        val startState = State(g, s)
+        val a = nextStates(startState).first()
+        return nextStates(a)
     }
 
 
