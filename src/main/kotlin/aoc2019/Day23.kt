fc58a8b9fd2bb5356418ecc359e7ed7f7a414cb1
isabel <isabel.wingen@innoq.com>
2022-12-06 16:02:54 +0100

    Solve 2019/Day23 in ugly

--- src/main/kotlin/aoc2019/Day23.kt
+++ src/main/kotlin/aoc2019/Day23.kt
@@ -20,7 +20,6 @@ class Day23: Day("23") {
         IntRange(0, 49).forEach { inputs[it] = LinkedList() }
 
         while (true) {
-            // execute programs with given input (or -1 if none is given). Write output to output stream
             for (prog in progs) {
                 val res = if (inputs[prog.first]!!.isEmpty()) {
                     prog.second.execute(true)
@@ -34,7 +33,6 @@ class Day23: Day("23") {
                     outputs[prog.first]!!.add(res)
                 }
             }
-            // write outputs to inputs
             outputs.forEach { entry ->
                 if (entry.value.size >= 3) {
                     val address = entry.value.pop()!!.toInt()
@@ -49,15 +47,66 @@ class Day23: Day("23") {
         }
     }
 
-    override fun expectedResultPart1(): Any {
-        TODO("Not yet implemented")
-    }
+    override fun expectedResultPart1() = 24268L
 
     override fun executePart2(name: String): Any {
-        TODO("Not yet implemented")
+        val progs = MutableList(50) { index -> index to IntCode("$index", name) }
+        progs.forEach { it.second.execute(it.first.toLong(), true) }
+        val outputs = HashMap<Int, LinkedList<Long>>()
+        val inputs = HashMap<Int, LinkedList<Long>>()
+        IntRange(0, 49).forEach { outputs[it] = LinkedList() }
+        IntRange(0, 49).forEach { inputs[it] = LinkedList() }
+
+        var cycle = 0
+        var idle = true
+        var nat = 0L to 0L
+        var lastSend = 0L
+
+        while (true) {
+            for (prog in progs) {
+                val res = if (inputs[prog.first]!!.isEmpty()) {
+                    prog.second.execute(true)
+                } else {
+                    val p = LinkedList<Long>()
+                    p.addAll(inputs[prog.first]!!)
+                    inputs[prog.first]!!.clear()
+                    prog.second.execute(p, true)
+                }
+                if (res != -123456789L) { // received output
+                    outputs[prog.first]!!.add(res)
+                }
+            }
+            outputs.forEach { entry ->
+                if (entry.value.size >= 3) {
+                    idle = false
+                    val address = entry.value.pop()!!.toInt()
+                    if (address == 255) {
+                        val a = entry.value.pop()!!
+                        val b = entry.value.pop()!!
+                        nat = a to b
+                    } else {
+                        inputs[address]!!.add(entry.value.pop()!!)
+                        inputs[address]!!.add(entry.value.pop()!!)
+                    }
+                }
+            }
+            if (cycle >= 1000) {
+                cycle = 0
+                if (idle) {
+                    inputs[0]!!.add(nat.first)
+                    inputs[0]!!.add(nat.second)
+                    if (lastSend == nat.second) {
+                        return nat.second
+                    } else {
+                        lastSend = nat.second
+                    }
+                }
+                idle = true
+            } else {
+                cycle += 1
+            }
+        }
     }
 
-    override fun expectedResultPart2(): Any {
-        TODO("Not yet implemented")
-    }
+    override fun expectedResultPart2() = 19316L
 }
