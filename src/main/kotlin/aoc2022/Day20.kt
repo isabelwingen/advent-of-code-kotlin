a470f0ca414fda35db63203356e1af28242a488e
isabel <isabel.wingen@innoq.com>
2023-06-13 12:44:57 +0200

    Improve time

--- src/main/kotlin/aoc2022/Day20.kt
+++ src/main/kotlin/aoc2022/Day20.kt
@@ -23,54 +23,67 @@ class Day20: Day("20") {
         }
     }
 
-    private fun moveElement(index: Int, list: LinkedList<Pair<Int, Long>>) {
+    private fun moveElement(index: Int, list: IntArray, values: LongArray): IntArray {
         val currentPosition = findPosition(index, list)
-        val value = list[currentPosition].second
+        val value = values[index]
         val newPosition = remainder(currentPosition + value, list.lastIndex)
-        list.removeAt(currentPosition)
-        list.add(newPosition, index to value)
+        val newList = IntArray(list.size) { list[it] }
+        return if (newPosition == currentPosition) {
+            newList
+        } else if ((newPosition == 0) && (newPosition == newList.lastIndex)) {
+            System.arraycopy(list, currentPosition + 1, newList, currentPosition, list.size - currentPosition - 1)
+            newList[newList.lastIndex] = index
+            newList
+        } else if (newPosition > currentPosition) {
+            System.arraycopy(list, currentPosition + 1, newList, currentPosition, newPosition - currentPosition)
+            newList[newPosition] = index
+            newList
+        } else {
+            newList[newPosition] = index
+            System.arraycopy(list, newPosition, newList, newPosition+1, currentPosition - newPosition)
+            newList
+        }
     }
 
-    private fun findPosition(index: Int, list: LinkedList<Pair<Int, Long>>): Int {
-        return list.mapIndexed { i, pair -> i to pair }
-            .first { it.second.first == index }
-            .first
+    private fun findPosition(index: Int, list: IntArray): Int {
+        for (i in list) {
+            if (list[i] == index) {
+                return i
+            }
+        }
+        throw IllegalStateException("Element not found")
     }
 
 
     override fun executePart1(name: String): Any {
-        val input = readInput(name).mapIndexed { index, i -> index to i }
-        val list = LinkedList<Pair<Int, Long>>()
-        list.addAll(input)
-        for (i in input.indices) {
-            moveElement(i, list)
+        val values = readInput(name)
+        var list = values.indices.toList().toIntArray()
+        for (i in values.indices) {
+            list = moveElement(i, list, values.toLongArray())
         }
         val posOfZero = list.mapIndexed { i, pair -> i to pair }
-            .first { it.second.second == 0L }
+            .first { values[it.second] == 0L }
             .first
-        val a = list[(posOfZero + 1000) % list.size].second
-        val b = list[(posOfZero + 2000) % list.size].second
-        val c = list[(posOfZero + 3000) % list.size].second
+        val a = values[list[(posOfZero + 1000) % list.size]]
+        val b = values[list[(posOfZero + 2000) % list.size]]
+        val c = values[list[(posOfZero + 3000) % list.size]]
         return a+b+c
     }
 
     override fun executePart2(name: String): Any {
-        val input = readInput(name)
-            .mapIndexed { index, i -> index to i * 811589153L }
-        val list = LinkedList<Pair<Int, Long>>()
-        list.addAll(input)
+        val values = readInput(name).map { it * 811589153 }.toLongArray()
+        var list = values.indices.toList().toIntArray()
         for (j in 0 until 10) {
-
-            for (i in input.indices) {
-                moveElement(i, list)
+            for (i in values.indices) {
+                list =moveElement(i, list, values)
             }
         }
         val posOfZero = list.mapIndexed { i, pair -> i to pair }
-            .first { it.second.second == 0L }
+            .first { values[it.second] == 0L }
             .first
-        val a = list[(posOfZero + 1000) % list.size].second
-        val b = list[(posOfZero + 2000) % list.size].second
-        val c = list[(posOfZero + 3000) % list.size].second
+        val a = values[list[(posOfZero + 1000) % list.size]]
+        val b = values[list[(posOfZero + 2000) % list.size]]
+        val c = values[list[(posOfZero + 3000) % list.size]]
         return a+b+c
     }
 }
