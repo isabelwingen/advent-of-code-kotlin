3dbe79298b8bc5b8e7b76b9d73c2f30721cf08ae
Isabel Wingen <isabel.wingen@innoq.com>
2023-11-30 12:43:02 +0100

    Improve 2018/15.1

--- src/main/kotlin/aoc2018/Day15.kt
+++ src/main/kotlin/aoc2018/Day15.kt
@@ -32,17 +32,11 @@ class Day15: Day("15") {
         return Triple(positions, elves, goblins)
     }
 
-    data class Area(val key: Int, val positions: Set<Pair<Int, Int>>, val doors: Map<Int, Set<Pair<Int, Int>>>) {
-        override fun toString(): String {
-            return positions.toString()
-        }
-    }
-
     data class Player(var position: Pair<Int, Int>,  var type: String = "elf", var life: Int = 200, var attackPower: Int = 3) {
         override fun toString() = "${if (type == "elf") "e" else "g"}(${position.first},${position.second}/${life}/${attackPower})"
     }
 
-    data class Playfield(val areas: Collection<Area>, var players: MutableList<Player>, var finished: Boolean = false)
+    data class Playfield(val area: Set<Pair<Int, Int>>, val players: MutableList<Player>, var finished: Boolean = false)
 
     private fun neighbors(p: Pair<Int, Int>): Set<Pair<Int, Int>> {
         val (row, col) = p
@@ -53,12 +47,12 @@ class Day15: Day("15") {
         if (openSpaces.isEmpty()) {
             return emptySet()
         }
-        val distance = playfield.areas.flatMap { it.positions }.associateWith { INF }.toMutableMap()
-        val prev = playfield.areas.flatMap { it.positions }.associateWith { mutableSetOf<Pair<Int, Int>>() }.toMutableMap()
+        val distance = playfield.area.associateWith { INF }.toMutableMap()
+        val prev = playfield.area.associateWith { mutableSetOf<Pair<Int, Int>>() }.toMutableMap()
         val playerPositions = playfield.players.filter { it.life > 0 }.map { it.position }.toSet();
         distance[unit.position] = 0
         val queue = LinkedList<Pair<Int, Int>>()
-        queue.addAll(playfield.areas.flatMap { it.positions }.toSet())
+        queue.addAll(playfield.area)
         while (queue.isNotEmpty()) {
             val u = queue.minByOrNull { distance[it]!! }!!
             queue.remove(u)
@@ -95,7 +89,6 @@ class Day15: Day("15") {
             playfield.finished = true
             return
         }
-        val allFields = playfield.areas.flatMap { it.positions }.toSet()
         val playerPositions = playfield.players.filter { it.life > 0 }.map { it.position }.toSet()
         val unitNeighbors = neighbors(unit.position)
         if (targets.any { unitNeighbors.contains(it.position) }) {
@@ -103,7 +96,7 @@ class Day15: Day("15") {
         } else {
             val openSpaces = targets.flatMap { neighbors(it.position) }
                 .asSequence()
-                .filter { allFields.contains(it) }
+                .filter { playfield.area.contains(it) }
                 .filterNot { playerPositions.contains(it) }
                 .sortedBy { it.second }
                 .sortedBy { it.first }
@@ -133,10 +126,9 @@ class Day15: Day("15") {
     }
 
     private fun sort(playfield: Playfield) {
-        playfield.players = playfield.players
-            .filter { it.life > 0 }
-            .sortedBy { it.position.second }
-            .sortedBy { it.position.first }.toMutableList()
+        playfield.players.removeIf { it.life <= 0 }
+        playfield.players.sortBy { it.position.second }
+        playfield.players.sortBy { it.position.first }
     }
 
     private fun turn(playfield: Playfield, unit: Player) {
@@ -157,26 +149,14 @@ class Day15: Day("15") {
 
     override fun executePart1(name: String): Any {
         val (positions, e, g) = parse(name)
-        val area = Area(1, positions, mapOf())
         val players = (e.map { Player(it) } + g.map { Player(it, "goblin") }).sortedBy { it.position.second }.sortedBy { it.position.first }
-        val playfield = Playfield(listOf(area), players.toMutableList())
+        val playfield = Playfield(positions, players.toMutableList())
         var c = -1
         while (!playfield.finished) {
             playOneRound(playfield)
             c++
         }
         return c.toLong() * (playfield.players.sumOf { it.life.toLong() })
-    }
-
-    private fun bla(name: String) {
-        val (positions, e, g) = parse(name)
-        val area1 = Area(1, (positions.filter { it.first <= 8 } + (9 to 6)).toSet(), mapOf(2 to setOf(9 to 16)))
-        val area2 = Area(
-            2,
-            (positions.filter { it.first in 9..26 }.filter { it.second in 0..17 } + (27 to 12) + (27 to 13) + (27 to 8)).toSet(),
-            mapOf(1 to setOf(9 to 16), 3 to setOf(21 to 11, 21 to 17), 4 to setOf(27 to 8)))
-        val area3 = Area(3, (positions.filter { it.first in 9..27 }.filter { it.second in 18..31 } + (21 to 11) + (21 to 17)).toSet(), mapOf(2 to setOf(21 to 11, 21 to 17)))
-        val area4 = Area(4, (positions.filter { it.first >= 27 }.filter { it.second <= 9 }).toSet(), mapOf(2 to setOf(27 to 8)))
     }
 
     override fun executePart2(name: String): Any {
